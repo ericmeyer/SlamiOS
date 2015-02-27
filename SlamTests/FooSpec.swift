effa4dcaaf82de43e5a4bc76602438ed4f624b66
Eric Meyer <emeyer@8thlight.com>
2015-02-27 14:24:05 -0600

    Set up tests

---
+++ SlamTests/FooSpec.swift
@@ -0,0 +1,12 @@
+import Quick
+import Nimble
+
+class FooSpec: QuickSpec {
+    
+    override func spec() {
+        it("is another test") {
+            expect(1).to(equal(1))
+        }
+    }
+    
+}
