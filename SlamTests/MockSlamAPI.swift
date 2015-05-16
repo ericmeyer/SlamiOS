22a03f64e525cbd5dffc7067a056420a797b7b6b
Eric Meyer <emeyer@8thlight.com>
2015-05-16 15:22:51 -0500

    Remove unused vars in MockSlamAPI

--- SlamTests/MockSlamAPI.swift
+++ SlamTests/MockSlamAPI.swift
@@ -2,8 +2,6 @@ import Slam
 
 class MockSlamAPI: SlamAPI {
     var matches: [Match] = []
-    var receivedGetCount = false
-    var receivedGetMatchAtIndex = false
     var receivedGetQueue = false
     var wasMatchAdded = false
     var addMatchSuccessful = true
