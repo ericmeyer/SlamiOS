bfd4a0ec24f1090f439a013df631d19e13082f6e
Eric Meyer <emeyer@8thlight.com>
2015-08-21 15:18:50 -0500

    [5] Add Google Sign In Pod

--- Podfile
+++ Podfile
@@ -1,7 +1,11 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '6.0'
 
-target 'SlamTests' do
+target "Slam" do
+  pod "Google/SignIn"
+end
+
+target "SlamTests" do
   use_frameworks!
   pod "Quick", "~> 0.3.0"
   pod "Nimble"
---
+++ Pods/Target Support Files/Pods-Slam/Pods-Slam-environment.h
@@ -0,0 +1,56 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Google/Core
+#define COCOAPODS_POD_AVAILABLE_Google_Core
+#define COCOAPODS_VERSION_MAJOR_Google_Core 1
+#define COCOAPODS_VERSION_MINOR_Google_Core 0
+#define COCOAPODS_VERSION_PATCH_Google_Core 7
+
+// Google/SignIn
+#define COCOAPODS_POD_AVAILABLE_Google_SignIn
+#define COCOAPODS_VERSION_MAJOR_Google_SignIn 1
+#define COCOAPODS_VERSION_MINOR_Google_SignIn 0
+#define COCOAPODS_VERSION_PATCH_Google_SignIn 7
+
+// GoogleAppUtilities
+#define COCOAPODS_POD_AVAILABLE_GoogleAppUtilities
+#define COCOAPODS_VERSION_MAJOR_GoogleAppUtilities 1
+#define COCOAPODS_VERSION_MINOR_GoogleAppUtilities 0
+#define COCOAPODS_VERSION_PATCH_GoogleAppUtilities 0
+
+// GoogleAuthUtilities
+#define COCOAPODS_POD_AVAILABLE_GoogleAuthUtilities
+#define COCOAPODS_VERSION_MAJOR_GoogleAuthUtilities 1
+#define COCOAPODS_VERSION_MINOR_GoogleAuthUtilities 0
+#define COCOAPODS_VERSION_PATCH_GoogleAuthUtilities 1
+
+// GoogleNetworkingUtilities
+#define COCOAPODS_POD_AVAILABLE_GoogleNetworkingUtilities
+#define COCOAPODS_VERSION_MAJOR_GoogleNetworkingUtilities 1
+#define COCOAPODS_VERSION_MINOR_GoogleNetworkingUtilities 0
+#define COCOAPODS_VERSION_PATCH_GoogleNetworkingUtilities 0
+
+// GoogleSignIn
+#define COCOAPODS_POD_AVAILABLE_GoogleSignIn
+#define COCOAPODS_VERSION_MAJOR_GoogleSignIn 2
+#define COCOAPODS_VERSION_MINOR_GoogleSignIn 2
+#define COCOAPODS_VERSION_PATCH_GoogleSignIn 0
+
+// GoogleSymbolUtilities
+#define COCOAPODS_POD_AVAILABLE_GoogleSymbolUtilities
+#define COCOAPODS_VERSION_MAJOR_GoogleSymbolUtilities 1
+#define COCOAPODS_VERSION_MINOR_GoogleSymbolUtilities 0
+#define COCOAPODS_VERSION_PATCH_GoogleSymbolUtilities 0
+
+// GoogleUtilities
+#define COCOAPODS_POD_AVAILABLE_GoogleUtilities
+#define COCOAPODS_VERSION_MAJOR_GoogleUtilities 1
+#define COCOAPODS_VERSION_MINOR_GoogleUtilities 0
+#define COCOAPODS_VERSION_PATCH_GoogleUtilities 1
+
