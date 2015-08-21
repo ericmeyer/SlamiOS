53068bf29615477eb5c51b12c304ffd84779ba02
Eric Meyer <emeyer@8thlight.com>
2015-08-21 14:41:47 -0500

    [5] Add empty login view controller

---
+++ SlamTests/Slam/LoginViewControllerSpec.swift
@@ -0,0 +1,14 @@
+import Slam
+import Quick
+import Nimble
+
+class LoginViewControllerSpec: QuickSpec {
+    override func spec() {
+
+        describe("The login controller") {
+            it("exists") {
+                let controller = LoginViewController()
+            }
+        }
+    }
+}
